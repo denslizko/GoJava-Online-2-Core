baa6bdb979155a94e10489f850d7022813ae35cb
denslizko <denis.slizko@gmail.com>
2016-03-09 21:00:54 +0200

    Added files via upload

---
+++ Module3/Task1/TextFile.java
@@ -0,0 +1,7 @@
+package com.goit.gojavaonline.Module3.Task1;
+
+/**
+ * Created by Денис on 09.03.2016.
+ */
+public class TextFile extends File {
+}
---
+++ Module3/Task3/MusicStore.java
@@ -0,0 +1,10 @@
+package com.goit.gojavaonline.Module3.Task3;
+
+import java.util.List;
+
+/**
+ * Created by Денис on 09.03.2016.
+ */
+public class MusicStore {
+    List<MusicalInstrument> musicalInstruments;
+}
